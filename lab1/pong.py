546795ba54955022c3b8030f11f8c8134699f67b
Valerie <valerie.gadapati@gmail.com>
2023-02-02 10:50:48 -0500

    more refining pong

--- lab1/pong.py
+++ lab1/pong.py
@@ -26,7 +26,7 @@ Y_BALL_INCREMENT = 1  # y distance component of balls movement
 
 # create a start screen that tells the user to hit space to start the game
 def start_screen():
-    global default, left_paddle_y, left_paddle_x, right_paddle_x, right_paddle_y
+    global default, left_paddle_y, left_paddle_x, right_paddle_x, right_paddle_y, ball_x, ball_y
 
     set_clear_color(0, 0, 0, 1)  # set background black
 
@@ -43,6 +43,10 @@ def start_screen():
         right_paddle_x = WINDOW_X - PADDLE_WIDTH
         right_paddle_y = WINDOW_Y - PADDLE_HEIGHT
 
+        # set initial ball location
+        ball_x = WINDOW_X // 2
+        ball_y = WINDOW_Y // 2
+
         # change state to false, so it only runs on start up
         default = False
 
@@ -51,9 +55,9 @@ def start_screen():
 def playing_screen():
     global ball_x, ball_y
 
-    # draw dotted divider line
     dash_y = 0
     dash_width = 5
+
     # for the length of the window draw evenly spaced rectangles
     while dash_y <= WINDOW_Y:
         dash_x = WINDOW_X // 2 - dash_width//2
@@ -79,6 +83,7 @@ def keypress(key):
 
     if key == START_KEY:
         playing = True
+        default = False
 
     if key == QUIT_KEY:
         playing = False
@@ -129,27 +134,31 @@ def move_ball():
     ball_y = ball_y + (Y_BALL_INCREMENT * 2)
 
 
-# define ball behavior when hitting various targets
-def ball_behavior():
+# define ball behavior when hitting walls
+def ball_wall_behavior():
     global default, playing, ball_x, ball_y, BALL_RADIUS, X_BALL_INCREMENT, Y_BALL_INCREMENT, right_paddle_x, right_paddle_y, left_paddle_x, left_paddle_y, PADDLE_HEIGHT, PADDLE_WIDTH
 
     # behavior to bounce off the horizontal top and bottom walls
     if ((ball_y - BALL_RADIUS) <= 0) or ((ball_y + BALL_RADIUS) >= WINDOW_Y):
         Y_BALL_INCREMENT = -Y_BALL_INCREMENT
 
-    # if the ball hits a vertical side wall reset the ball's position and direction
+    # if the ball hits a vertical side wall the game ends
     if ((ball_x + BALL_RADIUS) >= WINDOW_X) or ((ball_x-BALL_RADIUS) <= 0):
-        ball_x = WINDOW_X // 2
-        ball_y = WINDOW_Y // 2
-        X_BALL_INCREMENT = 1
-        Y_BALL_INCREMENT = 1
+        playing = False
+        default = True
+
+
+# define ball behavior when hitting walls
+def ball_paddle_behavior():
+    global ball_x, ball_y, BALL_RADIUS, X_BALL_INCREMENT, right_paddle_x, right_paddle_y, left_paddle_x, left_paddle_y, PADDLE_HEIGHT, PADDLE_WIDTH
 
     # if it hits the right paddle, flip the x component
     if (ball_x + BALL_RADIUS >= right_paddle_x) and (right_paddle_y <= ball_y <= right_paddle_y + PADDLE_HEIGHT):
         X_BALL_INCREMENT = -X_BALL_INCREMENT
 
     # if it hits the left paddle, flip the x component
-    if (ball_x - BALL_RADIUS <= left_paddle_x + PADDLE_WIDTH) and (left_paddle_y <= ball_y <= left_paddle_y + PADDLE_HEIGHT):
+    if (ball_x - BALL_RADIUS <= left_paddle_x + PADDLE_WIDTH) and (
+            left_paddle_y <= ball_y <= left_paddle_y + PADDLE_HEIGHT):
         X_BALL_INCREMENT = -X_BALL_INCREMENT
 
 
@@ -168,25 +177,18 @@ def graphics():
 
         draw_circle(ball_x, ball_y, BALL_RADIUS)
         move_ball()
-        ball_behavior()
+        ball_wall_behavior()
+        ball_paddle_behavior()
 
 
 # state variables
 playing = False
 default = True
 
-left_paddle_x = 0
-left_paddle_y = 0
-right_paddle_x = WINDOW_X - PADDLE_WIDTH
-right_paddle_y = WINDOW_Y - PADDLE_HEIGHT
-
 pressed_left_up = False
 pressed_left_down = False
 pressed_right_up = False
 pressed_right_down = False
-
-ball_x = WINDOW_X//2
-ball_y = WINDOW_Y//2
 
 # utilize callback functions to check keyboard inputs and facilitate gameplay
 start_graphics(graphics, 2400, key_press=keypress, key_release=keyrelease)
