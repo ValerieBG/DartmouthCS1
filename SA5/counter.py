01490e17bb72c1c32335e05b2afef008ed2e3081
Valerie <valerie.gadapati@gmail.com>
2023-02-02 10:57:46 -0500

    finished pong and working on SA5

--- SA5/counter.py
+++ SA5/counter.py
@@ -1,24 +1,25 @@
-### DOCUMENTATIOJN UP EHRE
-
-
-
+# filename: counter.py
+# author:   Valerie Gadapati
+# date:     Feb 6, 2023
+# purpose:  define a class that counts down
 
 class Counter:
     def __init__(self, limit, initial=0, min_digits=1):
         self.limit = limit
+        self.count = limit
 
         # If the initial value supplied as a parameter is between 0 and limit–1, then that's the counter's initial
         # value. Otherwise, an error message is printed and the counter's initial value is limit–1.
         if 0 < initial < limit - 1:
-            self.initial = initial
+            self.count = initial
         else:
             print("Error: initial value outside countdown range")
-            self.initial = limit - 1
+            self.count = limit - 1
         self.min_digits = min_digits
 
     # returns the counter's value, as an int.
     def get_value(self):
-        return self
+        return str(self)
 
     # returns the counter's value, as a string. If the counter's value would print with fewer than the min_digits
     # parameter supplied to the __init__ method, then the string contains min_digits characters, padded on the left with
@@ -26,7 +27,7 @@ class Counter:
     # For example, if min_digits is 4 and the counter's value is 15, then the method should return the string 0015.
     def __str__(self):
         if len(str(self)) < self.min_digits:
-            return (self.min_digits - str(self)) * '0' + self
+            return (self.min_digits - len(str(self.count))) * '0' + str(self.count)
         else:
             return self
 
@@ -36,9 +37,9 @@ class Counter:
     # i.e., did the value wrap from 0 back to limit–1. True means that it wrapped, False means that it did not.
     def tick(self):
         wrapped = False
-        self = self - 1
-        if self < 0:
-            self = self.limit - 1
+        self.count = self.count - 1
+        if self.count < 0:
+            self.count = self.limit - 1
             wrapped = True
         return wrapped
 
--- SA5/driver.py
+++ SA5/driver.py
@@ -1,11 +1,13 @@
-### DOCUMENTATIOJN UP EHRE
-
-
-
-
+# filename: driver.py
+# author:   Valerie Gadapati
+# date:     Feb 6, 2023
+# purpose:  create a class that makes a digital counter
 
 from counter import *
 
-testCounter = Counter(15, 3, 4)
-
-print(testCounter)
+testCounter = Counter(10, 5, 4)
+i = 0
+while i <= 10:
+    print(testCounter)
+    testCounter.tick()
+    i = i + 1
