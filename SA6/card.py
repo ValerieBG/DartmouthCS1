9092601595494977eff107698f0bffc734413fc1
Valerie <valerie.gadapati@gmail.com>
2023-02-06 14:00:01 -0500

    working on sa6... getting repeat cards after shuffling

--- SA6/card.py
+++ SA6/card.py
@@ -1,15 +1,15 @@
 # filename: card.py
 # author:   Valerie Gadapati
 # date:     Feb 12, 2023
-# purpose:  to create a playing cards with suits and names
+# purpose:  to create a playing cards with suits and values
 
 class Card:
     suits = ["clubs", "diamonds", "spades", "hearts"]
-    names = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
-    def __init__(self, name, suit):
-        self.name = Card.names[name - 1]
+    def __init__(self, value, suit):
+        self.value = Card.values[value - 1]
         self.suit = Card.suits[suit-1]
 
     def __str__(self):
-        return self.name + " of " + self.suit
+        return self.value + " of " + self.suit
--- SA6/deck.py
+++ SA6/deck.py
@@ -7,33 +7,33 @@ from card import *
 from random import *
 
 class Deck:
-    def __init__(self):
-        self.card_list = [Card] * 52  # DONT NEED 52
+    def __init__(self, length=52):
+        self.card_list = [Card] * length
 
     def add_standard_cards(self):
         i = 0
-        while i <= len(Card.suits) - 1:
+        while i < len(Card.suits):
             j = 0
-            while j <= len(Card.names) - 1:
+            while j < len(Card.values):
                 card = Card(j, i)
-                position = (i * len(Card.names)) + j
+                position = (i * len(Card.values)) + j
                 self.card_list[position] = card
                 j = j + 1
             i = i + 1
 
     def shuffle(self):
         i = 0
-        while i <= len(self.card_list) - 1:
+        while i < len(self.card_list):
             current_pos = i
-            new_pos = randint(0, len(self.card_list)-1)
+            new_pos = randint(0, len(self.card_list) - 1)
             temp = self.card_list[current_pos]
             self.card_list[new_pos] = temp
             i = i + 1
 
     def deal(self, hand_count):
-        hand = []
+        dealt = Deck(hand_count)
         i = 0
-        while i <= hand_count:
-            hand[i] = (self.card_list.pop())
-            i = i + 1
-        return hand
+        while i < len(dealt.card_list):
+            dealt.card_list[i] = self.card_list.pop()
+            i += 1
+        return dealt
