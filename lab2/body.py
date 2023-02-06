bc37980dfc27514b342f99e13f7795675e719676
Valerie <valerie.gadapati@gmail.com>
2023-02-06 11:52:51 -0500

    setting up for lab2

---
+++ lab2/body.py
@@ -0,0 +1,7 @@
+# filename: deck.py
+# author:   Valerie Gadapati
+# date:     Feb XX, 2023
+# purpose:  XX
+
+class Body:
+    def __init__(self):
---
+++ lab2/system.py
@@ -0,0 +1,8 @@
+# filename: system.py
+# author:   Valerie Gadapati
+# date:     Feb XX, 2023
+# purpose:  XX
+
+class System:
+    def __init__(self):
+
