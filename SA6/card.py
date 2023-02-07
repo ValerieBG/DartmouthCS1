f211bc0953efaf7edeccd88a4420915c4cc4240d
Valerie <valerie.gadapati@gmail.com>
2023-02-07 09:45:49 -0500

    sa6 done

--- SA6/card.py
+++ SA6/card.py
@@ -1,15 +1,17 @@
 # filename: card.py
 # author:   Valerie Gadapati
 # date:     Feb 12, 2023
-# purpose:  to create a playing cards with suits and values
+# purpose:  to create a playing card with suits and values
 
 class Card:
     suits = ["clubs", "diamonds", "spades", "hearts"]
     values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
+    # initialize a card with a value and suit as given by the parameters
     def __init__(self, value, suit):
         self.value = Card.values[value - 1]
         self.suit = Card.suits[suit-1]
 
+    # prints "value of suit" for a card object (e.g. 5 of clubs or Queen of hearts)
     def __str__(self):
         return self.value + " of " + self.suit
--- SA6/deck.py
+++ SA6/deck.py
@@ -1,15 +1,18 @@
 # filename: deck.py
 # author:   Valerie Gadapati
 # date:     Feb 12, 2023
-# purpose:  to create and manipulate a standard deck of cards, using the card class
+# purpose:  to create and manipulate a deck of cards, using the card class
 
 from card import *
 from random import *
 
+
 class Deck:
+    # initialize a deck of cards, default length is a standard deck of 52 cards
     def __init__(self, length=52):
         self.card_list = [Card] * length
 
+    # add standard 52 cards consisting of Ace, 2, 3 ... 10, Jack, Queen, King cards within each suit
     def add_standard_cards(self):
         i = 0
         while i < len(Card.suits):
@@ -21,15 +24,18 @@ class Deck:
                 j = j + 1
             i = i + 1
 
+    # function to randomly shuffle the deck based on card positions
     def shuffle(self):
         i = 0
         while i < len(self.card_list):
             current_pos = i
             new_pos = randint(0, len(self.card_list) - 1)
             temp = self.card_list[current_pos]
+            self.card_list[current_pos] = self.card_list[new_pos]
             self.card_list[new_pos] = temp
             i = i + 1
 
+    # deal a deck of hand_count length, and remove those cards from the original deck
     def deal(self, hand_count):
         dealt = Deck(hand_count)
         i = 0
