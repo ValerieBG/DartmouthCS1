01fcaa2cdca782bab7ec1a1a8df8caacefb28740
Valerie <valerie.gadapati@gmail.com>
2023-02-10 12:33:50 -0500

    lab2 prog

--- lab2/body.py
+++ lab2/body.py
@@ -6,9 +6,26 @@
 from cs1lib import *
 class Body:
     def __init__(self, mass, x, y, vx, vy, pixel_radius, r, g, b):
+        self.mass = mass
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.pixel_radius = pixel_radius
+        self.r = r
+        self.g = g
+        self.b = b
     def update_position(self, timestep):
+        self.x = self.x + self.vx * timestep
+        self.y = self.y + self.vy * timestep
+
     def update_velocity(self, ax, ay, timestep):
+        self.vx = self.vx + ax * timestep
+        self.vy = self.vy + ay * timestep
+
     def draw(self, cx, cy, pixels_per_meter):
-        draw_circle()
+        disable_stroke()
+        set_fill_color(self.r, self.g, self.b)
+        draw_circle(cx * pixels_per_meter, cy * pixels_per_meter, self.pixel_radius)
 
 
--- lab2/system.py
+++ lab2/system.py
@@ -6,14 +6,21 @@
 from body import Body
 
 class System:
-    def __init__(self, num_bodies):
-        bodies = [Body] * num_bodies
-
-#    def __init__(self, body_list):  # vasanta solution
+    def __init__(self, list_bodies):
+        self.bodies = list_bodies
+# computes the accelerations on each body and then calls methods in Body to update the velocity and position of each body.
     def update(self, timestep):
+        (ax, ay) = self.compute_acceleration(len(self.bodies))
+        for bod in self:
+            bod.update_position(timestep)
+            bod.update_velocity(ax, ay, timestep)
+
     def draw(self, cx, cy, pixels_per_meter):
+        for bod in self.bodies:
+            bod.draw(bod.x, bod.y, bod.pixels_per_meter)
 
     def compute_acceleration(self, n):
+
         return (a_x, a_y)  # return a tuple with two values, the accelerations in x and y
 
 
