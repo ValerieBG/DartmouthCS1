19aacd49c763a48e4aa56d36338b74548c5b82ff
Valerie <valerie.gadapati@gmail.com>
2023-02-19 13:10:08 -0500

    lab2 tweaks

--- lab2/solar.py
+++ lab2/solar.py
@@ -11,11 +11,11 @@ from body import Body
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 400
 
-TIME_SCALE = 100000         # real seconds per simulation second
-PIXELS_PER_METER = 1 / 1.5e9  # distance scale for the simulation
+TIME_SCALE = 3.0e6              # real seconds per simulation second
+PIXELS_PER_METER = 7 / 1e10     # distance scale for the simulation
 
-FRAMERATE = 30              # frames per second
-TIMESTEP = 1.0 / FRAMERATE  # time between drawing each frame
+FRAMERATE = 30                  # frames per second
+TIMESTEP = 1.0 / FRAMERATE      # time between drawing each frame
 
 
 def main():
@@ -32,13 +32,13 @@ def main():
 
 
 # source: https://nssdc.gsfc.nasa.gov/planetary/factsheet/, converted units as needed
-# sun has no velocity because it is the center of the system
+# sun is the center of the system
 sun = Body(1.98892e30, 0, 0, 0, 0, 20, 1, 1, 0)
 # the first four planets
-mercury = Body(0.330e24, 0, 57.9e9, 47400, 0, 3, 0.75, 0.75, 0.75)
-venus = Body(4.87e24, 0, 108.2e9, 35000, 0, 5, 240/255, 230/255, 140/255)
-earth = Body(5.9736e24, 0, 149.6e9, 29800, 0, 5, 0, 0, 1)
-mars = Body(0.642e24, 0, 228.0e9, 24100, 0, 10, 193/255, 68/255, 14/255)
+mercury = Body(0.330e24, -57.9e9, 0, 0, 47890, 3, 0.75, 0.75, 0.75)
+venus = Body(4.87e24, -108.2e9, 0, 0, 35040, 5, 240/255, 230/255, 140/255)
+earth = Body(5.9736e24, -149.6e9, 0, 0, 29790, 5, 0, 0, 1)
+mars = Body(0.642e24, -227.9e9, 0, 0, 24140, 10, 193/255, 68/255, 14/255)
 
 solar_sys = System([sun, mercury, venus, earth, mars])
 
