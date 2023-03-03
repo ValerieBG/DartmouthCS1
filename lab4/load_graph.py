deec4f85147369e373ad7bff826805910380307b
Valerie <valerie.gadapati@gmail.com>
2023-03-03 09:32:02 -0500

    lab4 checkpoint

--- lab4/load_graph.py
+++ lab4/load_graph.py
@@ -1,33 +1,41 @@
-## DOCU HERE
+# filename: load_graph.py
+# author:   Valerie Gadapati
+# date:     Mar 7, 2023
+# purpose:  parse a file to create a dictionary of locational information
+
 from vertex import Vertex
 
 
-# Your job is to write a function load_graph in load_graph.py, which takes one parameter, the name of the data file.
-# load_graph should create one Vertex object per line in the data file and add to a dictionary a reference to each
-# Vertex object it creates. The Vertex references (addresses) will be the values in the dictionary, and the
-# names of the vertices will be the keys for the dictionary. When done reading and processing all the information
-# in the data file, the function should return the address of the dictionary, which will have information about all
-# the vertices.
+# function to parse a data file and create a dictionary of vertices from it
 def load_graph(filename):
 
     vertex_dict = {}
 
-    # Read the lines in the file into a list of lines:
+    # read the data file
     data = open(filename, "r")
 
     for line in data:
+        # first split by ; into three categories
         split_params = line.split(";")
+
+        # identify the name of the vertex
         name = split_params[0].strip()
+
+        # create a list of the adjacent parameters
         adj_verts = split_params[1].strip().split(",")  # returns a list
         adj = []
         for x in adj_verts:
             adj.append(x.strip())
+
+        # identify the x and y coordinates
         coords = split_params[2].strip().split(",")
         x = coords[0].strip()
         y = coords[1].strip()
 
+        # add the new vertex to a dictionary using appropriate parameters
         vertex_dict[name] = Vertex(str(name), adj, int(x), int(y))
         # print(vertex_dict[name])  # for testing
+
     data.close()
     return vertex_dict
 
--- lab4/vertex.py
+++ lab4/vertex.py
@@ -1,13 +1,18 @@
-# DOCU
+# filename: vertex.py
+# author:   Valerie Gadapati
+# date:     Mar 7, 2023
+# purpose:  define a class Vertex that describes connected vertices and data
 
 class Vertex:
     def __init__(self, name, adj_verts, x, y):
-        self.adj_verts = adj_verts
         self.name = name
+        self.adj_verts = adj_verts
+        # x and y coordinates as ints
         self.x = x
         self.y = y
 
     def __str__(self):
+        # logic to not have a comma after the last adjacent vertex
         adj = ""
         for i in range(len(self.adj_verts)):
             elem = self.adj_verts[i]
@@ -16,4 +21,6 @@ class Vertex:
                 adj += ", "
             else:
                 adj += str(elem)
+
+        # returns a string in the format of Vertex Name; Location: X, Y; Adjacent vertices: Adj1, Adj2, ... , Adjx
         return self.name + "; " + "Location: " + str(self.x) + ", " + str(self.y) + "; " + "Adjacent vertices: " + adj
